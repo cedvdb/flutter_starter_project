57b2b06db8fd6f8e02ca7c66eec60768da6deab5
cedvdb <cedvandenbosch@gmail.com>
2021-01-05 12:50:37 +0100

    create restaurant page

--- lib/ui/screens/startup/home_screen/home_screen.dart
+++ lib/ui/screens/startup/home_screen/home_screen.dart
@@ -1,15 +1,19 @@
 import 'package:eureka_app/app-root/app_info.dart';
 import 'package:eureka_app/data/state/auth/auth_cubit.dart';
-import 'package:eureka_app/app-root/routes.dart';
 import 'package:eureka_app/ui/screens/startup/home_screen/drawer/drawer_content.dart';
 import 'package:eureka_app/ui/screens/startup/home_screen/drawer/drawer_layout.dart';
 import 'package:eureka_app/ui/screens/startup/home_screen/drawer/drawer_service.dart';
 import 'package:eureka_app/ui/widgets/layout_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 
 class HomeScreen extends StatelessWidget {
-  final nav = DrawerService(links: drawerLinks);
+  final DrawerService nav = GetIt.I.get<DrawerService>();
+
+  HomeScreen() {
+    nav.init();
+  }
 
   @override
   Widget build(BuildContext context) {
