f05a2e35281792a8891e1e6a647b23b746e5440e
cedvdb <cedvandenbosch@gmail.com>
2021-01-06 22:58:06 +0100

    single child scroll view on dashboard

--- lib/ui/screens/app/dashboard/dashboard_screen.dart
+++ lib/ui/screens/app/dashboard/dashboard_screen.dart
@@ -6,23 +6,25 @@ import 'bar_chart.dart';
 class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Card(
-        //   child: BlocBuilder<RestaurantCubit, RestaurantState>(
-        //     builder: (ctx, state) {
-        //       return Text(state.toString());
-        //     },
-        //   ),
-        // ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            color: Colors.white.withAlpha(50),
-            child: AppBarChart(),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          // Card(
+          //   child: BlocBuilder<RestaurantCubit, RestaurantState>(
+          //     builder: (ctx, state) {
+          //       return Text(state.toString());
+          //     },
+          //   ),
+          // ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              color: Colors.white.withAlpha(50),
+              child: AppBarChart(),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
